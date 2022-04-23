7e2cca1e395bfd25abed7455795c6854d8f643fb
Pablo Kvitca <pablokvitca@gmail.com>
2022-04-22 22:09:08 -0400

    stuff

--- Quantilizer.py
+++ Quantilizer.py
@@ -1,20 +1,32 @@
+from torch import zeros, full, logical_not, Tensor
+
+
 class Quantilizer:
     
     # Creates a quantilizer initialized at zero that will move specific amounts
     # when provided a new value 
-    def __init__(self, stepUp, stepDown):
-        self.stepUp = stepUp
-        self.stepDown = stepDown
+    def __init__(self, n_outputs, step_up, step_down, device='cuda'):
+        self.n_outputs = n_outputs
+        self.step_ups = full((n_outputs, ), step_up, device=device)
+        self.step_downs = full((n_outputs, ), step_down, device=device)
 
-        self.currStep = 0
+        self.curr_steps = zeros((n_outputs, ), device=device)
     
     # Checks if a value "passes" the quantilizer and moves the internal step counter accordingly
     # True if strictly greater than the current step, false otherwise
-    def check(self, val):
-        if val > self.currStep:
-            self.currStep += self.stepUp
-            
+    def check_negs(self, vals: Tensor, true_idx) -> Tensor:
+        mask_up = vals > self.curr_steps
+        mask_up[true_idx] = False
+        self.curr_steps[mask_up] += self.step_ups[mask_up]
+        mask_down = logical_not(mask_up)
+        mask_down[true_idx] = False
+        self.curr_steps[mask_down] -= self.step_downs[mask_down]
+        return mask_up
+
+    def check_pos(self, val, true_idx) -> bool:
+        if val > self.curr_steps[true_idx]:
+            self.curr_steps[true_idx] += self.step_ups[true_idx]
             return True
         else:
-            self.currStep -= self.stepDown
+            self.curr_steps[true_idx] -= self.step_downs[true_idx]
             return False
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import hydra
 import os
 from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
+from torch import where, Tensor
 
 import mnist
 import random
@@ -29,26 +30,30 @@ def main(cfg: DictConfig):
         tags=cfg.WANDB_TAGS.split(',')
     )
 
+    print("Initializing...")
+
     # Loading MNIST data
+    print("Loading data...")
     x_train, t_train, x_test, t_test = mnist.load(get_original_cwd())
 
-    x_train = torch.from_numpy(x_train)
-    t_train = torch.from_numpy(t_train)
-    x_test = torch.from_numpy(x_test)
-    t_test = torch.from_numpy(t_test)
+    x_train: Tensor = torch.from_numpy(x_train)
+    t_train: Tensor = torch.from_numpy(t_train)
+    x_test: Tensor = torch.from_numpy(x_test)
+    t_test: Tensor = torch.from_numpy(t_test)
 
     NUM_EXAMPLES_TRAIN = cfg.NUM_EXAMPLES_TRAIN if cfg.NUM_EXAMPLES_TRAIN > 0 else len(x_train)
     NUM_EXAMPLES_VAL = cfg.NUM_EXAMPLES_VAL if cfg.NUM_EXAMPLES_VAL > 0 else len(x_test)
+    print(f"Data Loaded. Using {NUM_EXAMPLES_TRAIN} training samples and {NUM_EXAMPLES_VAL} validation samples")
 
+    print("Preprocessing data...")
     FILTER_THRESHOLD = cfg.FILTER_THRESHOLD
 
     # Filters to keep anything at or _above_ the threshold value
     # Not that this turns into binary, so small intensities are equivalent to max intensity
-    # TODO: Why is this so slow to pre-process these? I should definitely pre-load these...
-    filtered_x_train = torch.where(x_train[:NUM_EXAMPLES_TRAIN] >= FILTER_THRESHOLD, 1, 0)
-    filtered_x_test = torch.where(x_test[:NUM_EXAMPLES_TRAIN] >= FILTER_THRESHOLD, 1, 0)
+    filtered_x_train = where(x_train[:NUM_EXAMPLES_TRAIN] >= FILTER_THRESHOLD, 1, 0)
+    filtered_x_test = where(x_test[:NUM_EXAMPLES_TRAIN] >= FILTER_THRESHOLD, 1, 0)
 
-    # import pdb; pdb.set_trace()
+    print("Preprocessing done.")
 
     # Hyper parameters
     NUM_EPOCHS = cfg.NUM_EPOCHS
@@ -66,32 +71,31 @@ def main(cfg: DictConfig):
     PRUNE_WEIGHT = cfg.PRUNE_WEIGHT
     LOG_LEVEL = logging._nameToLevel[cfg.LOG_LEVEL]
     DEVICE = ('cuda' if torch.cuda.is_available() else 'cpu') if cfg.DEVICE == 'auto' else cfg.DEVICE
+    print("RUNNING ON DEVICE:", DEVICE)
+
     RANDOM_SEED = cfg.RANDOM_SEED
 
-    # Make sure to set the random seed (should propogate to all other imports of random)
+    # Make sure to set the random seed (should propagate to all other imports of random)
     random.seed(RANDOM_SEED)
     torch.manual_seed(RANDOM_SEED)
     np.random.seed(RANDOM_SEED)
 
-    # Initializg model for MNIST
-    # model = SONN(784, 10)
-    model = BetterSONN(INPUT_SIZE,
-                        OUTPUT_SIZE,
-                        NUM_NEURONS_PER_COLUMN,
-                        NUM_CONNECTIONS_PER_NEURON,
-                        spike_threshold=SPIKE_THRESHOLD,
-                        max_update_threshold=MAX_UPDATE_THRESHOLD,
-                        initial_connection_weight=INITIAL_CONNECTION_WEIGHT,
-                        positive_reinforce_amount=POSITIVE_REINFORCE_AMOUNT,
-                        negative_reinforce_amount=NEGATIVE_REINFORCE_AMOUNT,
-                        decay_amount=DECAY_AMOUNT,
-                        prune_weight=PRUNE_WEIGHT,
-                        device=DEVICE)
+    # Initialing model for MNIST
+    model = BetterSONN(
+        INPUT_SIZE,
+        OUTPUT_SIZE,
+        NUM_NEURONS_PER_COLUMN,
+        NUM_CONNECTIONS_PER_NEURON,
+        spike_threshold=SPIKE_THRESHOLD,
+        max_update_threshold=MAX_UPDATE_THRESHOLD,
+        initial_connection_weight=INITIAL_CONNECTION_WEIGHT,
+        positive_reinforce_amount=POSITIVE_REINFORCE_AMOUNT,
+        negative_reinforce_amount=NEGATIVE_REINFORCE_AMOUNT,
+        decay_amount=DECAY_AMOUNT,
+        prune_weight=PRUNE_WEIGHT,
+        device=DEVICE
+    )
 
-    # old_model = SONN(784, 10)
-
-    # TODO: Make the hyperparameters settable out here, instead of just
-    # pulling the numbers from the other files (I'm lazy right now...)
     wandb.config = {
       "epochs": NUM_EPOCHS,
       "num_examples_train": NUM_EXAMPLES_TRAIN,
@@ -110,9 +114,10 @@ def main(cfg: DictConfig):
       "device": DEVICE
     }
 
+    print("Training...")
     for epoch in (epoch_pbar := tqdm(range(NUM_EPOCHS), position=0, leave=True)):
 
-        correctCountTrain = 0
+        correct_count_train = 0
         train_start = timer()
         num_pos_reinforces = 0
         num_neg_reinforces = 0
@@ -123,10 +128,6 @@ def main(cfg: DictConfig):
             y = t_train[i].item()
 
             pred, spikes, pos_reinforcements, neg_reinforcements = model.learn(x, y)
-            # pred2, spikes2 = old_model.learn(x.cpu().numpy(), y)
-
-            # if pred != 0:
-            #     import pdb; pdb.set_trace()
 
             num_pos_reinforces += len(pos_reinforcements)
             num_neg_reinforces += len(neg_reinforcements)
@@ -134,24 +135,22 @@ def main(cfg: DictConfig):
 
             # Counting how many we get correct
             if pred == y:
-                correctCountTrain += 1
-            # import pdb; pdb.set_trace()
+                correct_count_train += 1
 
-            # print("i:", i, "True:", y, "Prediction:", pred, "Spike Counts:", spikes)
         train_end = timer()
 
         train_time = train_end - train_start
         avg_train_time_per_example = train_time / NUM_EXAMPLES_TRAIN
 
         val_start = timer()
-        correctCountVal = validation(model, DEVICE, NUM_EXAMPLES_VAL, filtered_x_test, t_test[:NUM_EXAMPLES_VAL])
+        correct_count_val = validation(model, DEVICE, NUM_EXAMPLES_VAL, filtered_x_test, t_test[:NUM_EXAMPLES_VAL])
         val_end = timer()
 
         val_time = val_end - val_start
         avg_val_time_per_example = val_time / NUM_EXAMPLES_VAL
 
-        train_accuracy = correctCountTrain / NUM_EXAMPLES_TRAIN
-        val_accuracy = correctCountVal / NUM_EXAMPLES_VAL
+        train_accuracy = correct_count_train / NUM_EXAMPLES_TRAIN
+        val_accuracy = correct_count_val / NUM_EXAMPLES_VAL
 
         epoch_pbar.set_description(
             f"Epoch {str(epoch).zfill(len(str(NUM_EPOCHS)))} - train acc: {train_accuracy:.5f} - avg train time: "
@@ -164,14 +163,15 @@ def main(cfg: DictConfig):
         # Logging to W&B
         wandb.log({
             "epoch": epoch,
-            "train_accuracy": correctCountTrain / NUM_EXAMPLES_TRAIN,
-            "val_accuracy": correctCountVal / NUM_EXAMPLES_VAL,
+            "train_accuracy": correct_count_train / NUM_EXAMPLES_TRAIN,
+            "val_accuracy": correct_count_val / NUM_EXAMPLES_VAL,
             "train_time": train_time,
             "val_time": val_time,
             "positive_reinforces": num_pos_reinforces,
             "negative_reinforces": num_neg_reinforces,
         })
 
+    print("Done!")
 
 if __name__ == "__main__":
     main()
